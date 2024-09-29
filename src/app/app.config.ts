954d590e51e46f03810aa8bc8166859d8e9b5c34
Ahmed Selem <Ahmed.Selem@Transflo.com>
2024-09-29 18:05:59 +0300

    Initial commit 🚀

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
+    provideHttpClient(withFetch()),
+    {
+      provide: ErrorHandler,
+    },
+    provideAnimations(),
+  ],
 };
